bd15bdb2c784f2a867805066a6c6ad766c764e24
harryscully <harry.scully@ugogroup.co.uk>
2025-05-22 12:32:59 +0100

    Add "hover" event listener for each div

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const container = document.querySelector(".container");
 let generateDiv = function() {
     const div = document.createElement("div");
     container.appendChild(div);
+    div.addEventListener("mouseover", () => {
+        div.setAttribute("class", "hovered");
+    })
 };
 
 for (let i = 1; i <= 256; i++) {
