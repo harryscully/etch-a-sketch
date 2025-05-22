e4792eab8db0d89ee42c9cc3d55f62ac265de351
harryscully <harry.scully@ugogroup.co.uk>
2025-05-22 13:17:21 +0100

    Add button to allow user to change grid size

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ let generateDiv = function() {
     })
 };
 
-for (let i = 1; i <= 256; i++) {
+let n = 256;
+for (let i = 1; i <= n; i++) {
     generateDiv()
 };
+
+let gridButton = document.querySelector("button");
+
+gridButton.addEventListener("click", () => {
+    let n = prompt("Pick a value of n for nxn grid (max n=100)", 16);
+    if (n > 100) {
+        alert("Value for n too high");
+    } else {
+        while (container.div) {
+            container.removeChild(div)
+        };
+        for (let i = 1; i <= n**2; i++) {
+            generateDiv()
+        };
+    }
+})
